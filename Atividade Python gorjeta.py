b3a6a2309f2ec23ba0037e9d4831e49ae55d48b0
Lucasmkz <lucaspolicosta123@gmail.com>
2026-03-26 19:29:39 -0300

    Atividade Faculdade Python

---
+++ Atividade Python gorjeta.py
@@ -0,0 +1,6 @@
+valor_gasto = float(input("Informe o valor gasto: "))
+gorjeta = valor_gasto * 0.1
+valor_total = valor_gasto + gorjeta
+
+print(f"Valor da gorjeta: {gorjeta:.2f}")
+print(f"Total a pagar: {valor_total:.2f}")
